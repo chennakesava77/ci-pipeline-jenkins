817d952cc7001fba22e24829386a3bd3e53ec5e7
Kesava Reddy <chennakesavareddy.boreddy@gmail.com>
2025-10-17 15:05:48 +0000

    Tests passing locally

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,16 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.js'],
-  reporters: ['default', 'jest-junit']
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: 'test-results',
+      outputName: 'junit.xml',
+      suiteName: 'jest tests',
+      classNameTemplate: '{classname}-{title}',
+      titleTemplate: '{title}',
+      ancestorSeparator: ' › ',
+      usePathForSuiteName: 'true'
+    }]
+  ]
 };
